d1a578f9c2c518828a01c25d4fe1d972985c5594
Kontik <81539288+Lekrixsss@users.noreply.github.com>
2022-09-30 22:11:06 +0000

    Update codecs.sh

--- scripts/codecs.sh
+++ scripts/codecs.sh
@@ -1,21 +1,23 @@
 #bash
 
 #Check is RPMFusion repo installed
-if sudo dnf list rpmfusion-free-appstream-data | grep -q 'rpmfusion-free-appstream-data.noarch'; then
-  echo "RPMFusion already installed"
-    #Codecs
-    echo "Installing codecs"
-    sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
-    sudo dnf install -y lame\* --exclude=lame-devel
-    sudo dnf group upgrade -y --with-optional Multimedia
+main(){
+  if sudo dnf list rpmfusion-free-appstream-data | grep -q 'rpmfusion-free-appstream-data.noarch'; then
+    echo "RPMFusion already installed"
+      #Codecs
+      echo "Installing codecs"
+      sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
+      sudo dnf install -y lame\* --exclude=lame-devel
+      sudo dnf group upgrade -y --with-optional Multimedia
 
-    #Tainted
-    tained="Install additional codecs from tainted repo? (May be restricted in some countries) (y/N): "
-      if [ "$tainted" = y ]
-      then
-      echo "Installing codecs from tainted"
-        sudo dnf install rpmfusion-free-release-tainted
-        sudo dnf install libdvdcss
+      #Tainted
+      tained="Install additional codecs from tainted repo? (May be restricted in some countries) (y/N): "
+        if [ "$tainted" = y ]
+        then
+        echo "Installing codecs from tainted"
+          sudo dnf install rpmfusion-free-release-tainted
+          sudo dnf install libdvdcss
+}
 fi
 echo "Enabling the RPM Fusion repositories"
 sudo dnf install -y \
@@ -24,4 +26,4 @@ sudo dnf install -y \
   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf group update core
 
-done
+main
