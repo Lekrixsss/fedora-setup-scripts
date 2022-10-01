28f59dc7b7eeaa1b792668382e43f726b920c8d0
Kontik <81539288+Lekrixsss@users.noreply.github.com>
2022-10-01 00:51:13 +0000

    Add chromium freeworld option to cdecs script

--- scripts/codecs.sh
+++ scripts/codecs.sh
@@ -22,9 +22,15 @@ then
   read -p "Install additional codecs from tainted repo? (May be restricted in some countries) (y/N): " tainted
   if [ "$tainted" = y ]
   then
-  echo "Installing codecs from tainted"
+  echo "Installing codecs from tainted repo"
     sudo dnf install -y rpmfusion-free-release-tainted
     sudo dnf install -y libdvdcss
+  fi
+  #Freeworld
+  read -p "Do you want to install chromium freeworld(Chromium built with all freeworld codecs and VA-API support)? (y/N) " freeworld
+  if [ "$freeworld = y"]
+  then
+  sudo dnf install -y chromium-freeworld
   fi
 else
 rpmfusion_ins
