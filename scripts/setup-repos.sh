4c801f86ed24a4d9629df22f575363183b6ce434
Lekrixsss <atix.atix6@gmail.com>
2022-10-01 03:11:25 +0300

    Fix repos script

--- scripts/setup-repos.sh
+++ scripts/setup-repos.sh
@@ -1,5 +1,8 @@
-sudo dnf install \
+echo "Enabling the RPM Fusion repositories"
+sudo dnf install -y \
   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install \
+sudo dnf install -y \
   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf group update core
+echo "Enabling the Flathub repositorie"
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
